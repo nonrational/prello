f52b787eeb835cf40a3c7a25f4075bac30f8d028
Alan Norton <me@alannorton.com>
2015-08-19 10:27:27 -0400

    working with more oo stuff

--- index.js
+++ index.js
@@ -2,34 +2,35 @@ var
   rest = require('restler'),
   util = require('util'),
   nconf = require('nconf').env(),
-  p = console.log,
-  Trello = require('node-trello'),
-  t = new Trello(nconf.get("TRELLO_KEY"), nconf.get("TRELLO_SECRET"));
+  Trello = require('node-trello');
 
-var pr=undefined;
+var pr = undefined;
 
 var owner = 'nonrational', repo = 'prello', number = 2;
 var pr_url=util.format("https://api.github.com/repos/%s/%s/pulls/%s", owner, repo, number);
 rest.get(pr_url)
   .on('complete', function(r){
     pr = r;
-    process(pr.body);
+    var card = new TrelloCard(t, extract_card_id(pr.body));
+    card.comment(util.format("[prello] says _hello_ from %s", pr.html_url))
+
+    console.log(card.url);
   })
 
-function process(body){
-  var linked = /https?:\/\/(www\.)?trello\.com\/c\/([^\/]+)/gi.exec(body);
-
-  if(linked){
-    var card_id = linked[2];
-    var comment_url = util.format("/1/cards/%s/actions/comments", card_id);
-
-    var comment_text = util.format("[prello] says _hello_ from %s", pr.html_url);
-
-    t.post(comment_url, { text: comment_text }, function(e,d){ });
-
-  } else {
-    console.err("trello link not found? probably no big deal.");
+function extract_card_id(text){
+  var card_url = /https?:\/\/(www\.)?trello\.com\/c\/([^\/]+)/gi.exec(text);
+  if(card_url){
+    return card_url[2];
   }
-
 }
 
+function TrelloCard(id){
+  this.id = id;
+  this.url = "https://trello.com/c/" + this.id;
+  this.conn = new Trello(nconf.get("TRELLO_KEY"), nconf.get("TRELLO_SECRET");
+}
+
+TrelloCard.prototype.comment = function(comment_text){
+  var comment_url = util.format("/1/cards/%s/actions/comments", this.id);
+  this.conn.post(comment_url, { text: comment_text }, function(e,d){ })
+}
