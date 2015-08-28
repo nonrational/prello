f8dc4a9cab1daeecf29d208581e0624c3dbaea32
Alan Norton <me@alannorton.com>
2015-08-28 17:12:54 -0400

    test success by creating/deleting an actual comment on a card

--- spec/lib/prello-spec.js
+++ spec/lib/prello-spec.js
@@ -2,25 +2,27 @@
 // spec/calculator-spec.js
 
 var fs = require('fs'),
-    prello = require("../../lib/prello");
+    prello = require("../../lib/prello"),
+    TrelloCard = require("../../lib/trello-card");
 
 var pr_sans_trello = JSON.parse(fs.readFileSync('./spec/fixtures/pr_sans_trello.json', 'utf8').toString());
 var pr_with_trello = JSON.parse(fs.readFileSync('./spec/fixtures/pr_with_trello.json', 'utf8').toString());
 var non_pr_event   = JSON.parse(fs.readFileSync('./spec/fixtures/non_pr_event.json', 'utf8').toString());
+var pr_with_blank_trello = JSON.parse(fs.readFileSync('./spec/fixtures/pr_with_blank_trello.json', 'utf8').toString());
 
 
 var Context = function(done, status, message) {
 
     this.succeed = function(d){
         expect(status).toBe("success");
-        expect(d).toBe(message);
-        done();
+        // console.log(d);
+        done(d);
     };
 
     this.fail = function(e){
         expect(status).toBe("failure");
         expect(e).toBe(message);
-        done();
+        done(e);
     };
 };
 
@@ -34,6 +36,21 @@ describe("handler", function() {
     it("should fail when no trello card referenced", function(done) {
         var context = new Context(done, "failure", "no trello card referenced on https://github.com/nonrational/prello/pull/2");
         prello.handler(pr_sans_trello, context);
+    });
+
+    it("should succeed on a blank card", function(done){
+        var comment_id;
+
+        var context = new Context(function(new_comment){
+            new TrelloCard("XinwRYra").delete_comment(new_comment.id, done);
+        }, "success");
+
+        prello.handler({
+          "pull_request": {
+            "html_url": "xxxxxxxxx",
+            "body": "https://trello.com/c/XinwRYra/4-test-card"
+          }
+        }, context);
     });
 
 });
