8391f2c1c033a6414444125ff588c63cdd95908a
Alan Norton <me@alannorton.com>
2015-08-28 16:42:42 -0400

    finally figured out how to use jasmine2.0

--- spec/lib/prello-spec.js
+++ spec/lib/prello-spec.js
@@ -8,41 +8,32 @@ var pr_sans_trello = JSON.parse(fs.readFileSync('./spec/fixtures/pr_sans_trello.
 var pr_with_trello = JSON.parse(fs.readFileSync('./spec/fixtures/pr_with_trello.json', 'utf8').toString());
 var non_pr_event   = JSON.parse(fs.readFileSync('./spec/fixtures/non_pr_event.json', 'utf8').toString());
 
-var Context = function(){
-    this.status = null;
-    this.message = null;
 
-    this.succeed = function(s){
-        this.status = "success";
-        this.message = s;
+var Context = function(done, status, message) {
+
+    this.succeed = function(d){
+        expect(status).toBe("success");
+        expect(d).toBe(message);
+        done();
     };
+
     this.fail = function(e){
-        this.status = "failure";
-        this.message = e;
+        expect(status).toBe("failure");
+        expect(e).toBe(message);
+        done();
     };
 };
 
+describe("handler", function() {
 
-describe("handler", function () {
-
-    it("should fail when trello card already has a comment", function() {
-        var ctx = new Context();
-
-        runs(function(){
-            prello.handler(pr_with_trello, ctx);
-        });
-
-        waitsFor(function(){
-            return ctx.status === "failure" && ctx.message === "comment text already present";
-        }, "status should be failure with text present", 1000);
+    it("should fail when trello card already has a comment", function(done) {
+        var context = new Context(done, "failure", "comment text already present");
+        prello.handler(pr_with_trello, context);
     });
 
-    it("should error when no trello card referenced", function() {
-        // this works because there's no async call in this path
-        var ctx = new Context();
-        prello.handler(pr_sans_trello, ctx);
-        expect(ctx.status).toBe("failure");
-        expect(ctx.message).toBe("no trello card referenced on https://github.com/nonrational/prello/pull/2");
+    it("should fail when no trello card referenced", function(done) {
+        var context = new Context(done, "failure", "no trello card referenced on https://github.com/nonrational/prello/pull/2");
+        prello.handler(pr_sans_trello, context);
     });
 
 });
