b20f37f9ef276f0187d24ac7d354ba9293c3b926
Alan Norton <me@alannorton.com>
2015-08-27 23:49:09 -0400

    make the same comment at most once

--- local.js
+++ local.js
@@ -4,7 +4,6 @@
 var index = require('./index.js'),
     fs = require('fs');
 
-// fs.readFile('./spec/fixtures/add_pr_comment.json', 'utf8', function(e,data){
 fs.readFile('./spec/fixtures/reopen_pr.json', 'utf8', function(e,data){
     index.handler(JSON.parse(data), {
         succeed: function(s){
